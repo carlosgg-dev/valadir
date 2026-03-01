1804c59c8587ef9ecd5ee00e4415765476f6f82f
Carlos <carlosgg.dev@gmail.com>
2026-03-01 22:19:51 +0100

    feat: initialize multi-module project structure

---
+++ valadir-boot/src/main/java/com/valadir/ValadirApplication.java
@@ -0,0 +1,13 @@
+package com.valadir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValadirApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ValadirApplication.class, args);
+    }
+}
