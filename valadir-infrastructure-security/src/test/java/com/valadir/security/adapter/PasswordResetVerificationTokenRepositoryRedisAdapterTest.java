3c7e082569dd2bb128edbd2e758109b2e9cc4ea7
Carlos <carlosgg.dev@gmail.com>
2026-08-31 18:32:59 +0200

    test: use RedisTestUtils.errorTemplate() for Redis error simulation

--- valadir-infrastructure-security/src/test/java/com/valadir/security/adapter/PasswordResetVerificationTokenRepositoryRedisAdapterTest.java
+++ valadir-infrastructure-security/src/test/java/com/valadir/security/adapter/PasswordResetVerificationTokenRepositoryRedisAdapterTest.java
@@ -2,78 +2,48 @@ package com.valadir.security.adapter;
 
 import com.valadir.common.exception.InfrastructureException;
 import com.valadir.domain.model.AccountId;
-import org.junit.jupiter.api.BeforeEach;
+import com.valadir.test.redis.RedisTestUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.ValueOperations;
 
 import java.time.Duration;
 
 import static com.valadir.security.redis.CircuitGuards.buildClosedCircuitGuard;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willThrow;
 
-@ExtendWith(MockitoExtension.class)
 class PasswordResetVerificationTokenRepositoryRedisAdapterTest {
 
     private static final AccountId ACCOUNT_ID = AccountId.generate();
     private static final String TOKEN = "some-verification-token";
 
-    private static final DataAccessException REDIS_ERROR = new DataAccessException("Redis down") {
-    };
-
-    @Mock
-    private RedisOperations<String, String> redisOperations;
-
-    @Mock
-    private ValueOperations<String, String> valueOperations;
-
-    private PasswordResetVerificationTokenRepositoryRedisAdapter adapter;
-
-    @BeforeEach
-    void setUp() {
-
-        adapter = new PasswordResetVerificationTokenRepositoryRedisAdapter(redisOperations, buildClosedCircuitGuard());
-    }
-
     @Test
     void save_redisUnavailable_throwsInfrastructureException() {
 
         var tokenTtl = Duration.ofMinutes(10);
-
-        given(redisOperations.opsForValue()).willReturn(valueOperations);
-        willThrow(REDIS_ERROR).given(valueOperations).set(any(), any(), any(Duration.class));
+        var adapter = new PasswordResetVerificationTokenRepositoryRedisAdapter(RedisTestUtils.errorTemplate(), buildClosedCircuitGuard());
 
         assertThatExceptionOfType(InfrastructureException.class)
             .isThrownBy(() -> adapter.save(TOKEN, ACCOUNT_ID, tokenTtl))
-            .withCause(REDIS_ERROR);
+            .withCauseInstanceOf(DataAccessException.class);
     }
 
     @Test
     void resolveAccountId_redisUnavailable_throwsInfrastructureException() {
 
-        given(redisOperations.opsForValue()).willReturn(valueOperations);
-        given(valueOperations.get(any())).willThrow(REDIS_ERROR);
+        var adapter = new PasswordResetVerificationTokenRepositoryRedisAdapter(RedisTestUtils.errorTemplate(), buildClosedCircuitGuard());
 
         assertThatExceptionOfType(InfrastructureException.class)
             .isThrownBy(() -> adapter.resolveAccountId(TOKEN))
-            .withCause(REDIS_ERROR);
+            .withCauseInstanceOf(DataAccessException.class);
     }
 
     @Test
     void delete_redisUnavailable_throwsInfrastructureException() {
 
-        given(redisOperations.delete(anyString())).willThrow(REDIS_ERROR);
+        var adapter = new PasswordResetVerificationTokenRepositoryRedisAdapter(RedisTestUtils.errorTemplate(), buildClosedCircuitGuard());
 
         assertThatExceptionOfType(InfrastructureException.class)
             .isThrownBy(() -> adapter.delete(TOKEN))
-            .withCause(REDIS_ERROR);
+            .withCauseInstanceOf(DataAccessException.class);
     }
 }
--- valadir-infrastructure-security/src/test/java/com/valadir/security/adapter/RateLimiterRedisAdapterTest.java
+++ valadir-infrastructure-security/src/test/java/com/valadir/security/adapter/RateLimiterRedisAdapterTest.java
@@ -1,48 +1,26 @@
 package com.valadir.security.adapter;
 
 import com.valadir.common.exception.InfrastructureException;
-import org.junit.jupiter.api.BeforeEach;
+import com.valadir.test.redis.RedisTestUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.RedisOperations;
 
 import java.time.Duration;
 
 import static com.valadir.security.redis.CircuitGuards.buildClosedCircuitGuard;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)
 class RateLimiterRedisAdapterTest {
 
     private static final Duration WINDOW = Duration.ofSeconds(60);
 
-    private static final DataAccessException REDIS_ERROR = new DataAccessException("Redis error") {
-    };
-
-    @Mock
-    private RedisOperations<String, String> redisOperations;
-
-    private RateLimiterRedisAdapter adapter;
-
-    @BeforeEach
-    void setUp() {
-
-        adapter = new RateLimiterRedisAdapter(redisOperations, buildClosedCircuitGuard());
-    }
-
     @Test
     void consume_redisError_throwsInfrastructureException() {
 
-        given(redisOperations.execute(any(), anyList(), any(), any(), any())).willThrow(REDIS_ERROR);
+        var adapter = new RateLimiterRedisAdapter(RedisTestUtils.errorTemplate(), buildClosedCircuitGuard());
 
         assertThatExceptionOfType(InfrastructureException.class)
             .isThrownBy(() -> adapter.consume("rate_limit:ip:test", 10, WINDOW))
-            .withCause(REDIS_ERROR);
+            .withCauseInstanceOf(DataAccessException.class);
     }
 }
